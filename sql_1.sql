78f8cbf8fbedb07181b803f483828158b25b370f
Max Larionov <maxim.larionov@flatstack.com>
2016-10-18 09:21:43 +0300

    select by name, not ID

--- sql_1.sql
+++ sql_1.sql
@@ -1,6 +1,8 @@
 /* What the students do not go on a course #101? */
 SELECT student.* FROM students student
+  LEFT OUTER JOIN courses course
+    WHERE course_name = "#101"
   LEFT OUTER JOIN students_courses students_course
     ON students_course.student_id = student.id
-    AND students_course.course_id = 1
+    AND students_course.course_id IN courses.id
       WHERE students_course.course_id IS NULL;
