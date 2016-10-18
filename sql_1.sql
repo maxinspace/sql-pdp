4aaca5058b1422d3d6ace011ddd1e4c3762a84d6
Max Larionov <maxim.larionov@flatstack.com>
2016-10-18 09:22:46 +0300

    Update sql_1.sql

--- sql_1.sql
+++ sql_1.sql
@@ -1,7 +1,6 @@
 /* What the students do not go on a course #101? */
 SELECT student.* FROM students student
-  LEFT OUTER JOIN courses course
-    WHERE course_name = "#101"
+  LEFT OUTER JOIN courses course ON course.name = "#101"
   LEFT OUTER JOIN students_courses students_course
     ON students_course.student_id = student.id
     AND students_course.course_id IN courses.id
